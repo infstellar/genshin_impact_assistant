9d5d00d85a098c14e953eb636de2a52915f1e0c8
infstellar <98541813+infstellar@users.noreply.github.com>
2022-10-17 22:58:46 +0800

    fix: base_threading

---
+++ source/base_threading.py
@@ -0,0 +1,29 @@
+import threading
+
+
+
+class Base_Threading(threading.Thread):
+    def __init__(self):
+        super().__init__()
+        self.pause_threading_flag=False
+        self.stop_threading_flag=False
+        
+    def pause_threading(self):
+        self.pause_threading_flag=True
+        
+    def continue_threading(self):
+        self.pause_threading_flag=False
+    
+    def stop_threading(self):
+        self.stop_threading_flag=True
+        
+    def checkup_stop_threading(self):
+        if self.stop_threading_flag:
+            return True
+    
+    def checkup_stop_func(self):
+        if self.pause_threading_flag or self.stop_threading_flag:
+            return True
+        
+    def run():
+        pass
