6cf14d64a251996d467d2f62f9fd1dd08c7ea10c
infstellar <98541813+infstellar@users.noreply.github.com>
2022-10-20 22:55:04 +0800

    code refactoring

--- source/domain_flow.py
+++ source/domain_flow.py
@@ -1,21 +1,24 @@
 import pyautogui
-from source import img_manager
+
+from base_threading import Base_Threading
 from unit import *
 
 import combat_loop, get_domain_reward, pdocr_api, text_manager as textM, interaction_background, posi_manager as PosiM, movement, config
 import flow_state as ST
-import threading
+import threading, img_manager
 
-class Domain_Flow_Control(threading.Thread):
+class Domain_Flow_Control(Base_Threading):
     @logger.catch
     def __init__(self):
         threading.Thread.__init__(self)
-        self.current_state=ST.STATE_BEFORE_CHALLENGE
+        self.current_state=ST.BEFORE_MOVETO_CHALLENGE
         self.itt=interaction_background.Interaction_BGD()
         chara_list=combat_loop.get_chara_list()
-        self.stop_flag=False
+        self.stop_threading_flag=False
         self.combatloop=combat_loop.Combat_Loop(chara_list,super_stop_func=self.get_stop_flag)
         self.gdr=get_domain_reward.Get_Reward()
+        self.gdr.setDaemon(True)
+        self.combatloop.setDaemon(True)
         self.domaininitflag=False
         self.automatic_start=False
         self.combatloop.pause_threading()
@@ -26,47 +29,48 @@ class Domain_Flow_Control(threading.Thread):
         self.last_domain_times=domain_times-1
         logger.info('秘境次数：' + str(domain_times))
     
-    def stop_thread(self):
-        self.stop_flag=True
+    def stop_threading(self):
+        logger.info('停止自动秘境')
+        self.gdr.stop_thread()
+        self.combatloop.stop_threading()
+        self.stop_threading_flag=True
     
-    def checkupstop(self):
-        if self.stop_flag:
-            logger.info('停止自动秘境')
-            self.gdr.stop_thread()
-            self.combatloop.stop_threading()
+    def checkup_stop_func(self):
+        if self.stop_threading_flag:
+            
             return True
     
     def get_stop_flag(self):
-        return self.stop_flag
+        return self.stop_threading_flag
     
     def autostartinit(self):
-        self.current_state=ST.STATE_BEFORE_CHALLENGE
+        self.current_state=ST.BEFORE_MOVETO_CHALLENGE
         self.domaininitflag=False
         self.automatic_start=True
     
-    def _state_check(self):
+    def _state_check(self): # Not in using
         cap=self.itt.capture()
         cap=self.itt.png2jpg(cap, channel='ui')
-        if self.current_state == ST.STATE_BEFORE_CHALLENGE:
-            for funcitem in [self._Trigger_READY_CHALLENGE]:
+        if self.current_state == ST.BEFORE_MOVETO_CHALLENGE:
+            for funcitem in [self._Trigger_AFTER_MOVETO_CHALLENGE]:
                 if funcitem(cap):
-                    self.current_state=ST.READY_CHALLENGE
+                    self.current_state=ST.AFTER_MOVETO_CHALLENGE
                     
-        elif self.current_state == ST.STATE_IN_CHALLENGE:
-            for funcitem in [self._Trigger_END_CHALLENGE]:
+        elif self.current_state == ST.IN_CHALLENGE:
+            for funcitem in [self._Trigger_AFTER_CHALLENGE]:
                 if funcitem(cap):
                     self.current_state=ST.END_CHALLENGE
             
-        elif self.current_state == ST.STATE_GETTING_REAWARD:
+        elif self.current_state == ST.IN_GETTING_REAWARD:
             for funcitem in [self._Trigger_GETTING_REAWARD]:
                 if funcitem(cap):
-                    self.current_state=ST.READY_GETTING_REAWARD
+                    self.current_state=ST.AFTER_GETTING_REAWARD
             
-        elif self.current_state == ST.STATE_END_COPY:
+        elif self.current_state == ST.END_DOMAIN:
             pass
         
     
-    def _Trigger_READY_CHALLENGE(self,cap):
+    def _Trigger_AFTER_MOVETO_CHALLENGE(self,cap):
         
         if pdocr_api.ocr.getTextPosition(self.itt.crop_image(cap,PosiM.posi_domain['Start']),
                                          textM.text(textM.start_challenge))!=-1:
@@ -74,7 +78,7 @@ class Domain_Flow_Control(threading.Thread):
         else:
             return False
     
-    def _Trigger_END_CHALLENGE(self,cap):
+    def _Trigger_AFTER_CHALLENGE(self,cap):
         if pdocr_api.ocr.getTextPosition(self.itt.crop_image(cap,PosiM.posi_domain['LeavingIn']), 
                                          textM.text(textM.LeavingIn))!=-1:
             return True
@@ -82,7 +86,7 @@ class Domain_Flow_Control(threading.Thread):
             return False
         
     
-    def _Trigger_GETTING_REAWARD(self,cap):
+    def _Trigger_GETTING_REAWARD(self,cap): # Not in using
         if pdocr_api.ocr.getTextPosition(self.itt.crop_image(cap,PosiM.posi_domain['ClaimRewards']), 
                                          textM.text(textM.claim_rewards))!=-1:
             return True
@@ -93,108 +97,106 @@ class Domain_Flow_Control(threading.Thread):
     def run(self):
         cap=self.itt.capture()
         cap=self.itt.png2jpg(cap,channel='ui')
-        # self.current_state=ST.STATE_GETTING_REAWARD
-        # self.domaininitflag=True
         while(1):
             
-            if self.checkupstop():
+            if self.checkup_stop_threading():
+                self.stop_threading()
+                time.sleep(10)
                 break
+            
             # if self.domaininitflag==False:
                 
             #     continue
-            self._state_check()
-            if self.current_state==ST.STATE_BEFORE_CHALLENGE:
-                
-                if self.domaininitflag==False:
-                    logger.info('正在开始挑战秘境')
-                    movement.reset_view()
-                    
-                    while(1):
-                        if self.checkupstop():
-                            break
-                        if pdocr_api.ocr.getTextPosition(cap, textM.text(textM.clld)) != -1:
-                            break
-                        if self.itt.get_img_existence(imgname=img_manager.IN_DOMAIN):
-                            break
-                        time.sleep(1)
-                        cap=self.itt.capture(jpgmode=2)
-                    
-                    cap=self.itt.capture(jpgmode=2)
+            # self._state_check()
+            
+            if self.current_state==ST.INIT_MOVETO_CHALLENGE:
+                logger.info('正在开始挑战秘境')
+                movement.reset_view()
+                while(1):
+                    if self.checkup_stop_func():
+                        break
                     if pdocr_api.ocr.getTextPosition(cap, textM.text(textM.clld)) != -1:
-                        self.itt.move_to(PosiM.posi_domain['CLLD'][0],PosiM.posi_domain['CLLD'][1])
-                        time.sleep(1)
-                        pyautogui.leftClick()
-                    time.sleep(5)
-                    movement.view_to_angle(-90)
-                    if self.checkupstop():
                         break
-                    # time.sleep(2)
-                    if self.checkupstop():
+                    if self.itt.get_img_existence(imgname=img_manager.IN_DOMAIN):
                         break
-
-                    self.domaininitflag=True
-                    self.itt.keyDown('w')
-                    if self.checkupstop():
-                        break
-                    time.sleep(5)
-                    if self.checkupstop():
-                        break
-                    
-                    
+                    time.sleep(1)
+                    cap=self.itt.capture(jpgmode=2)
+                
+                cap=self.itt.capture(jpgmode=2)
+                if pdocr_api.ocr.getTextPosition(cap, textM.text(textM.clld)) != -1:
+                    self.itt.move_to(PosiM.posi_domain['CLLD'][0],PosiM.posi_domain['CLLD'][1])
+                    time.sleep(1)
+                    pyautogui.leftClick()
+                time.sleep(5)
                 movement.view_to_angle(-90)
-                movement.move(movement.AHEAD,3)
+                
+                
+                
+                self.current_state==ST.BEFORE_MOVETO_CHALLENGE
+            
+            elif self.current_state==ST.BEFORE_MOVETO_CHALLENGE:
+                self.itt.keyDown('w')
+                if self.checkup_stop_func():
+                    break
+                time.sleep(5)
+                self.current_state==ST.IN_MOVETO_CHALLENGE
+            
+            elif self.current_state==ST.IN_MOVETO_CHALLENGE:
+                
+                if self.checkup_stop_func():
+                    break
+                movement.view_to_angle(-90)
+                movement.move(movement.AHEAD,4)
                 time.sleep(0.08)
                 
-            
-            elif self.current_state==ST.READY_CHALLENGE:
+                if self._Trigger_AFTER_MOVETO_CHALLENGE():
+                    self.current_state=ST.INIT_CHALLENGE
+                
+            elif self.current_state==ST.INIT_CHALLENGE:
                 self.itt.keyUp('w')
-                # self.itt.keyDown('s')
-                # time.sleep(0.5)
-                # self.itt.keyUp('s')
-                # time.sleep(2)
-                
-                if self.combatloop.pause_threading_flag==True:
-                    logger.info('正在开始战斗')
-                    self.combatloop.continue_threading()
-                
+                logger.info('正在开始战斗')
+                self.combatloop.continue_threading()
                 self.itt.keyPress('f')
                 time.sleep(0.1)
-                self.current_state=ST.STATE_IN_CHALLENGE
                 
-            elif self.current_state==ST.STATE_IN_CHALLENGE:
+                self.current_state==ST.IN_CHALLENGE
                 
-                if self.combatloop.pause_threading_flag==True:
-                    logger.info('正在开始战斗')
-                    self.combatloop.continue_threading()
+            elif self.current_state==ST.IN_CHALLENGE:
+                
                 time.sleep(3)
+                if self._Trigger_AFTER_CHALLENGE():
+                    self.current_state==ST.AFTER_CHALLENGE
                     
-            elif self.current_state==ST.END_CHALLENGE:
+            elif self.current_state==ST.AFTER_CHALLENGE:
                 logger.info('正在停止战斗')
                 self.combatloop.pause_threading()
                 time.sleep(5)
                 logger.info('等待岩造物消失')
                 time.sleep(20)
-                self.current_state=ST.STATE_GETTING_REAWARD
-                
-            elif self.current_state==ST.STATE_GETTING_REAWARD:
-                self.gdr.reset_flag()
-                self.gdr.continue_thread()
-                logger.info('正在激活石化古树')
-                while(1):
-                    if self.checkupstop():
-                        break
-                    time.sleep(1)
-                    if self.gdr.get_statement()==False:
-                        self.current_state=ST.STATE_END_COPY
-                        break
+                self.current_state=ST.END_CHALLENGE
             
-            elif self.current_state==ST.STATE_END_COPY:
+            elif self.current_state==ST.END_CHALLENGE:
+                self.current_state==ST.INIT_GETTING_REAWARD
+            
+            elif self.current_state==ST.INIT_GETTING_REAWARD:
+                # self.gdr.reset_flag()
+                self.gdr.continue_threading()
+                logger.info('正在激活石化古树')
+                self.current_state==ST.IN_GETTING_REAWARD
+                    
+            elif self.current_state==ST.IN_GETTING_REAWARD:
+                time.sleep(3)
+                if self.gdr.get_working_statement()==False:
+                    self.current_state=ST.END_GETTING_REAWARD
+            
+            elif self.current_state==ST.END_GETTING_REAWARD:
                 logger.info('秘境结束。')
                 # logger.info('domain over. restart next domain in 5 sec.')
-                if self.checkupstop():
-                    break
+                self.current_state==ST.END_DOMAIN
+            
+            elif self.current_state==ST.END_DOMAIN:
                 time.sleep(5)
-                if self.checkupstop():
+                if self.checkup_stop_func():
                     break
                 cap=self.itt.capture()
                 cap=self.itt.png2jpg(cap, channel='ui')
@@ -211,10 +213,10 @@ class Domain_Flow_Control(threading.Thread):
                     time.sleep(0.5)
                     self.itt.leftClick()
                     self.autostartinit()
-                    if self.checkupstop():
+                    if self.checkup_stop_func():
                         break
                     time.sleep(5)
-                    if self.checkupstop():
+                    if self.checkup_stop_func():
                         break
                 else:
                     logger.info('次数结束。退出秘境')
@@ -229,7 +231,8 @@ class Domain_Flow_Control(threading.Thread):
                     # exit all threads
                     self.gdr.stop_thread()
                     self.combatloop.stop_threading()
-                    
+                    self.stop_threading()
+                    time.sleep(10)
                     break
                 
 
--- source/listening.py
+++ source/listening.py
@@ -2,7 +2,7 @@ try:
     from unit import *
 except:
     from source.unit import *
-import PyHook3, time, keyboard
+import time, keyboard
 import alpha_loop, domain_flow
 combat_flag=False
 domain_flag=False
@@ -20,6 +20,7 @@ def switch_combat_loop():
     else:
         logger.info('启动自动战斗')
         t1=alpha_loop.Alpha_Loop()
+        t1.setDaemon(True)
         t1.start()
     combat_flag = not combat_flag
 # @logger.catch    
@@ -30,61 +31,20 @@ def switch_domain_loop():
         t2.stop_thread()
     else:
         logger.info('启动自动秘境')
-        t2=domain_flow.Domain_Flow_Control() 
+        t2=domain_flow.Domain_Flow_Control()
+        t2.setDaemon(True)
         t2.start()
     domain_flag = not domain_flag
     
 keyboard.add_hotkey(keymapjson["autoCombat"], switch_combat_loop)
 keyboard.add_hotkey(keymapjson["autoDomain"], switch_domain_loop)
 
-
-# data = []
-
 @logger.catch
 def listening():
-    # import pythoncom
-    # pythoncom.PumpMessages()
     while(1):
         time.sleep(0.1)
 
 
 if __name__ == '__main__':
-
     # 循环监听
     listening()
-    
-    
-# def OnKeyboardEvent(event):
-#               #同上，共同属性不再赘述
-#     #print('Message:',event.Message)
-#     # print('Time:',event.Time)
-#     # print('Window:',event.Window)
-#     # print('WindowName:',event.WindowName)
-#     # print('Ascii:', event.Ascii, chr(event.Ascii))   #按键的ASCII码
-#                            #按键的名称
-#     # print('KeyID:', event.KeyID)                     #按键的虚拟键值
-#     #print('ScanCode:', event.ScanCode)               #按键扫描码
-#     # print('Extended:', event.Extended)               #判断是否为增强键盘的扩展键
-#     # print('Injected:', event.Injected)
-#     # print('Alt', event.Alt)                          #是某同时按下Alt
-#     #print('Transition', event.Transition)            #判断转换状态
-#     # print('---')
-#     #print('Key:', event.Key) 
-    
-#     if event.Key=='Oem_2' and event.MessageName=='key down':
-#         logger.debug('MessageName: '+event.MessageName)
-#         logger.debug('Key:', event.Key)  
-#         switch_combat_loop()
-        
-#     if event.Key=='Oem_6' and event.MessageName=='key down':
-#         logger.debug('MessageName: '+event.MessageName)
-#         logger.debug('Key: '+ event.Key)  
-#         switch_domain_loop()
-
-#   # 同上
-#     return True
-
-
-# hm = PyHook3.HookManager()  # 创建一个HOOK管理对象
-# hm.KeyDown = OnKeyboardEvent # 绑定键盘处理函数--就是我们创建的函数
-# hm.HookKeyboard()   # 初始化
