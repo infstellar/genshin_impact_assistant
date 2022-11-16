4f9a7e041a66c8209bba54fb7a11aee1e17cbe7d
hsn2 <912372447@qq.com>
2022-11-17 03:58:08 +0800

    加入启动
    Former-commit-id: 2343ef3c6ff1016d7b771b765e35d1e95e58c4ed

--- genshin_assistant.py
+++ genshin_assistant.py
@@ -1,7 +1,10 @@
+from pywebio import platform
+
 import source.unit
-import win32api, win32con, win32gui, pyautogui, requests, unittest, xml, inspect, keyboard # 用于pyinstaller，不要删除！
+
 source.unit.logger.info('正在初始化，请稍后')
 # source.unit.logger.info('Initializing, please hold on')
+from source import webio
 
 try:
     import source.listening
@@ -11,5 +14,7 @@ except Exception as error:
     input('程序暂停。按任意键退出')
 
 source.unit.logger.info('初始化完成')
+
+platform.tornado.start_server(webio.main, auto_open_webbrowser=True, debug=source.unit.DEBUG_MODE)
 # source.unit.logger.info('Initialization Completed')
 source.listening.listening()
