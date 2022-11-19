6a931e7f2a7f8fcdb3053629e8d87803ba9157f4
hsn2 <912372447@qq.com>
2022-11-19 22:56:42 +0800

    移动函数:is_number
    Former-commit-id: 5b2380fd6cedd916e2c2de3da090e0f9a789b26a

---
+++ source/unil.py
@@ -0,0 +1,19 @@
+def is_number(s):
+    """
+    懒得写,抄的
+    https://www.runoob.com/python3/python3-check-is-number.html
+    """
+    try:
+        float(s)
+        return True
+    except ValueError:
+        pass
+
+    try:
+        import unicodedata
+        unicodedata.numeric(s)
+        return True
+    except (TypeError, ValueError):
+        pass
+
+    return False
