627fa498ef132c5a66dedfbbf28779b9c954f17a
hsn2 <912372447@qq.com>
2022-11-22 20:21:37 +0800

    setup update readmd change
    Former-commit-id: baa3d650ed228031a5f1a1831f349e8b1bbc3580

--- setup.py
+++ setup.py
@@ -6,10 +6,12 @@ sys.argv.pop(0)
 if len(sys.argv) == 1:
     if sys.argv[0] == 'install':
         if not os.path.exists('DTISTREQ'):
-            subprocess.check_call([sys.executable, '-m', 'pip', 'install', '-r', 'requirements.txt'])
+            subprocess.check_call([sys.executable, '-m', 'pip', 'install', '-r', 'requirements.txt', '-i',
+                                   'https://pypi.tuna.tsinghua.edu.cn/simple'])
             open('DTISTREQ', 'x')
     elif sys.argv[0] == 'update':
         subprocess.check_call(['git', 'pull'])
-        subprocess.check_call([sys.executable, '-m', 'pip', 'install', '-r', 'requirements.txt'])
+        subprocess.check_call([sys.executable, '-m', 'pip', 'install', '-r', 'requirements.txt', '-i',
+                               'https://pypi.tuna.tsinghua.edu.cn/simple'])
         if not os.path.exists('DTISTREQ'):
             open('DTISTREQ', 'x')
