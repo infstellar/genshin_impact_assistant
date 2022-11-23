a6c2eed6bbfb1e46db3bfe6641bcbc4f5b29483a
hsn2 <912372447@qq.com>
2022-11-23 20:27:09 +0800

    加入注释

--- source/webio/webio.py
+++ source/webio/webio.py
@@ -1,3 +1,4 @@
+import logging
 import sys
 import threading
 import time
@@ -5,6 +6,7 @@ import time
 import pywebio.session
 
 from source import webio
+from source.webio import log_handler
 from source.webio.pages import *
 
 status = True
@@ -13,8 +15,13 @@ status = True
 def main():
     webio.manager.reg_page('Main', MainPage())
     webio.manager.reg_page('Setting', SettingPage())
-
     webio.manager.load_page('Main')
+
+
+'''    handler = log_handler.WebioHandler()
+
+    logging.getLogger().addHandler(handler)'''
+
 '''    t = threading.Thread(target=session_check_thread, daemon=False)
     pywebio.session.register_thread(t)
     t.start()
@@ -36,7 +43,6 @@ def session_check_thread():
         pywebio.session.get_current_session()
         status = True
         time.sleep(0.1)'''
-
 
 if __name__ == '__main__':
     platform.tornado.start_server(main, auto_open_webbrowser=True, debug=True)
