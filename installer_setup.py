ebb601073749456b90c5055ee8e155cfdd81b733
infstellar <98541813+infstellar@users.noreply.github.com>
2022-11-30 12:32:19 +0800

    auto_setup
    Former-commit-id: a2638a1fd8285b862e62e52fa12a19fa90a40174

---
+++ installer_setup.py
@@ -0,0 +1,15 @@
+import source.config
+import os
+
+def unzip():
+    if not os.path.exists(os.path.join('source', 'cvAutoTrack_7.2.3', 'CVAUTOTRACK.dll')):
+        import zipfile
+        file = zipfile.ZipFile(os.path.join('source', 'cvAutoTrack_7.2.3', 'CVAUTOTRACK.zip'))
+        file.extractall(os.path.join('source', 'cvAutoTrack_7.2.3'))
+        file.close()
+        return 'unzip successfully'
+    return 'no operation required'
+def auto_setup():
+    print(unzip())
+    print(source.config.template_translator())
+    print(source.config.template_translator())
