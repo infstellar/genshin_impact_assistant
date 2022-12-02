1b222973581cd267fded0a2034bb6f551d6c40c0
hsn2 <912372447@qq.com>
2022-12-02 21:19:06 +0800

    change

--- installer_setup.py
+++ installer_setup.py
@@ -2,14 +2,16 @@ from source.util import *
 import source.config
 import os
 
+
 def unzip():
     if not os.path.exists(os.path.join('source', 'cvAutoTrack_7.2.3', 'CVAUTOTRACK.dll')):
         import zipfile
-        file = zipfile.ZipFile(os.path.join('source', 'cvAutoTrack_7.2.3', 'CVAUTOTRACK.zip'))
-        file.extractall(os.path.join('source', 'cvAutoTrack_7.2.3'))
-        file.close()
+        with zipfile.ZipFile(os.path.join('source', 'cvAutoTrack_7.2.3', 'CVAUTOTRACK.zip')) as f:
+            f.extractall(os.path.join('source', 'cvAutoTrack_7.2.3'))
         return 'unzip successfully'
     return 'no operation required'
+
+
 def auto_setup():
     print(unzip())
     print(source.config.template_translator())
