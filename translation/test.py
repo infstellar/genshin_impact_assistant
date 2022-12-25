b7b94cbe2b97af316d86da72e0f43aa60bcf18de
infstellar <98541813+infstellar@users.noreply.github.com>
2022-12-25 18:37:42 +0800

    feat: translation
    Former-commit-id: e891a2e16babcd258436e48321bb09f74b40b31c

--- translation/test.py
+++ translation/test.py
@@ -8,5 +8,9 @@ print(_("Hello world!."))
 
 # python pygettext.py -d zh_CN -p locale/zh_CN/LC_MESSAGES/zh_CN.po test.py
 # python pygettext.py -d en_US -p locale/zh_CN/LC_MESSAGES/en_US.po test.py
-# python msgfmt.py -o locale/zh_CN/LC_MESSAGES/zh_CN.mo locale/zh_CN/LC_MESSAGES/zh_CN.po
-# python msgfmt.py -o locale/en_US/LC_MESSAGES/en_US.mo locale/en_US/LC_MESSAGES/en_US.po
+# python msgfmt.py -o locale/zh_CN/LC_MESSAGES/zh_CN.mo locale/zh_CN/LC_MESSAGES/zh_CN.pot
+# python msgfmt.py -o locale/en_US/LC_MESSAGES/en_US.mo locale/en_US/LC_MESSAGES/en_US.pot
+
+
+# python pygettext.py -d zh_CN -p locale/zh_CN/LC_MESSAGES ../source/*.py
+# python pygettext.py -d en_US -p locale/en_US/LC_MESSAGES ../source/*.py
