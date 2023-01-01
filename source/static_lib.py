3980513375dd95c379550be19becc9e5ce756357
infstellar <98541813+infstellar@users.noreply.github.com>
2023-01-01 16:56:55 +0800

    opt: static lib; opt: cvautotrackerloop load process
    Former-commit-id: b2bda979148abd5ca077355c2518e722a75b7c70

--- source/static_lib.py
+++ source/static_lib.py
@@ -5,8 +5,10 @@ import numpy as np
 
 from timer_module import Timer
 from util import *
-import cvAutoTrack
 
+global W_KEYDOWN, cvAutoTrackerLoop
+W_KEYDOWN = False
+cvAutoTrackerLoop = None
 
 
 def get_handle():
@@ -18,95 +20,13 @@ def get_handle():
         return handle
 
 
-class ScreenCapture:
-    def __init__(self):
-        logger.debug('static_lib_created')
-
-        self.GetDC = ctypes.windll.user32.GetDC
-        self.CreateCompatibleDC = ctypes.windll.gdi32.CreateCompatibleDC
-        self.GetClientRect = ctypes.windll.user32.GetClientRect
-        self.CreateCompatibleBitmap = ctypes.windll.gdi32.CreateCompatibleBitmap
-        self.SelectObject = ctypes.windll.gdi32.SelectObject
-        self.BitBlt = ctypes.windll.gdi32.BitBlt
-        self.SRCCOPY = 0x00CC0020
-        self.GetBitmapBits = ctypes.windll.gdi32.GetBitmapBits
-        self.DeleteObject = ctypes.windll.gdi32.DeleteObject
-        self.ReleaseDC = ctypes.windll.user32.ReleaseDC
-        self.PostMessageW = ctypes.windll.user32.PostMessageW
-        self.MapVirtualKeyW = ctypes.windll.user32.MapVirtualKeyW
-        self.VkKeyScanA = ctypes.windll.user32.VkKeyScanA
-        self.handle = get_handle()
-
-        self.fps = 1 / 30
-        self.cap_timer = Timer()
-        self.wrtting_flag = False
-        self.last_cap1 = self.capture_handle()
-        self.last_cap2 = self.capture_handle()
-        self.ret_cap_id = 1
-        self.caping_flag = False
-        self.caping = self.capture_handle()
-        self.cap_timer.reset()
-
-    def capture_handle(self):
-        # 获取窗口客户区的大小
-        handle = self.handle
-        r = RECT()
-        self.GetClientRect(handle, ctypes.byref(r))
-        width, height = r.right, r.bottom
-        # 开始截图
-        dc = self.GetDC(handle)
-        cdc = self.CreateCompatibleDC(dc)
-        bitmap = self.CreateCompatibleBitmap(dc, width, height)
-        self.SelectObject(cdc, bitmap)
-        self.BitBlt(cdc, 0, 0, width, height, dc, 0, 0, self.SRCCOPY)
-        # 截图是BGRA排列，因此总元素个数需要乘以4
-        total_bytes = width * height * 4
-        buffer = bytearray(total_bytes)
-        byte_array = ctypes.c_ubyte * total_bytes
-        self.GetBitmapBits(bitmap, total_bytes, byte_array.from_buffer(buffer))
-        self.DeleteObject(bitmap)
-        self.DeleteObject(cdc)
-        self.ReleaseDC(handle, dc)
-        # 返回截图数据为numpy.ndarray
-        ret = np.frombuffer(buffer, dtype=np.uint8).reshape(height, width, 4)
-        return ret
-
-    def get_capture(self):
-        # 摆烂了
-        return self.capture_handle().copy()
-        
-        # if self.cap_timer.get_diff_time() >= self.fps:
-        #     self.cap_timer.reset()
-        #     # print('recap', self.cap_timer.getDiffTime())
-        #     if self.ret_cap_id == 1:
-        #         self.last_cap2 = self.capture_handle().copy()
-        #         self.ret_cap_id = 2
-        #     else:
-        #         self.last_cap1 = self.capture_handle().copy()
-        #         self.ret_cap_id = 1
-        #     print(self.cap_timer.get_diff_time())    
-            
-        # else:
-        #     # print('recap', self.cap_timer.getDiffTime())
-        #     # self.last_cap = self.capture_handle()
-        #     # self.cap_timer.reset()
-        #     print('returncap')
-        #     pass
-        # # print(self.last_cap.shape)
-        # # print(self.ret_cap_id)
-        # if self.ret_cap_id == 1:
-        #     return self.last_cap1
-        # else:
-        #     return self.last_cap2
-
-
-SCREENCAPTURE = ScreenCapture()
-W_KEYDOWN = False
-
-cvAutoTrackerLoop = cvAutoTrack.AutoTrackerLoop()
-cvAutoTrackerLoop.setDaemon(True)
-cvAutoTrackerLoop.start()
-time.sleep(1)
+def static_lib_init():
+    global W_KEYDOWN, cvAutoTrackerLoop
+    import cvAutoTrack
+    cvAutoTrackerLoop = cvAutoTrack.AutoTrackerLoop()
+    cvAutoTrackerLoop.setDaemon(True)
+    cvAutoTrackerLoop.start()
+    time.sleep(1)
 
 def while_until_no_excessive_error(stop_func):
     while cvAutoTrackerLoop.is_in_excessive_error():
@@ -114,18 +34,9 @@ def while_until_no_excessive_error(stop_func):
             return 0
         time.sleep(1)
 
-class TestTest(threading.Thread):
-    def __init__(self):
-        super().__init__()
-
-    def run(self):
-        while 1:
-            SCREENCAPTURE.get_capture()
+static_lib_init()
 
 
 if __name__ == '__main__':
     import cv2
     while_until_no_excessive_error()
-    while 1:
-        cv2.imshow("123",SCREENCAPTURE.get_capture())
-        cv2.waitKey(100)
