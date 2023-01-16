f15fb4275e1f8b1d85ddde85e8f75bb370548f39
infstellar <98541813+infstellar@users.noreply.github.com>
2023-01-16 19:12:16 +0800

    feat: GenericEvent

--- source/static_lib.py
+++ source/static_lib.py
@@ -1,5 +1,5 @@
-import threading
-from ctypes.wintypes import RECT
+from base_threading import BaseThreading
+from interaction_background import InteractionBGD
 
 import numpy as np
 
@@ -9,7 +9,7 @@ from util import *
 global W_KEYDOWN, cvAutoTrackerLoop
 W_KEYDOWN = False
 cvAutoTrackerLoop = None
-
+itt = InteractionBGD()
 
 def get_handle():
     if not config_json["cloud_genshin"]:
@@ -24,13 +24,53 @@ def get_handle():
         if handle != 0:
             return 331454
 
+class GenericEvent(BaseThreading):
+    
+    def __init__(self):
+        super().__init__()
+        self.w_down_timer = Timer()
+        self.w_down_flag = False
+    
+    def run(self):
+        '''if you're using this class, copy this'''
+        while 1:
+            time.sleep(self.while_sleep)
+            if self.stop_threading_flag:
+                return 0
 
+            if self.pause_threading_flag:
+                if self.working_flag:
+                    self.working_flag = False
+                time.sleep(1)
+                continue
+
+            if not self.working_flag:
+                self.working_flag = True
+                
+            if self.checkup_stop_func():
+                self.pause_threading_flag = True
+                continue
+            '''write your code below'''
+            if W_KEYDOWN == True:
+                if self.w_down_flag == False:
+                    self.w_down_flag = True
+                    self.w_down_timer.reset()
+                if self.w_down_timer.get_diff_time() >= 15:
+                    itt.key_down('w')
+                    logger.debug("static lib keydown: w")
+            else:
+                if self.w_down_flag == True:
+                    self.w_down_flag = False
+                    itt.key_up('w')
 def static_lib_init():
     global W_KEYDOWN, cvAutoTrackerLoop
     import cvAutoTrack
     cvAutoTrackerLoop = cvAutoTrack.AutoTrackerLoop()
     cvAutoTrackerLoop.setDaemon(True)
     cvAutoTrackerLoop.start()
+    generic_event = GenericEvent()
+    generic_event.setDaemon(True)
+    generic_event.start()
     time.sleep(1)
 
 def while_until_no_excessive_error(stop_func):
