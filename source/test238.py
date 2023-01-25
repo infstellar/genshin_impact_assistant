008acc13cfd0c8379935599a2bfca707391577a2
infstellar <98541813+infstellar@users.noreply.github.com>
2023-01-25 22:20:45 +0800

    some fixes
    Former-commit-id: 1fdabdf8d2dc7ce2021698d0d0046927d46a6d0e

---
+++ source/test238.py
@@ -0,0 +1,31 @@
+from util import *
+from collector_lib import get_item_id
+# save_json(load_json(json_name="icon.json", default_path="assets\\POI_JSON_API"),json_name="icon.json", default_path="assets\\POI_JSON_API")
+
+def reshape_json():
+    for i in list(range(1,14))+["area", "icon", "id_stamp", "item", "type"]:
+        save_json(load_json(json_name=str(i)+".json", default_path="assets\\POI_JSON_API\\zh_CN\\dataset"),json_name=str(i)+".json", default_path="assets\\POI_JSON_API\\zh_CN\\dataset")
+        logger.info(f"{i} succ")
+
+def create_indexes():
+    s = {}
+    for ii in range(1,14):
+        j = load_json(json_name=str(ii)+".json", default_path="assets\\POI_JSON_API\\zh_CN\\dataset")
+        for i in j:
+            s.setdefault(i["markerTitle"], []).append(str(ii))
+            s[i["markerTitle"]] = list(set(s[i["markerTitle"]]))
+        print(s)
+    save_json(s, "ID_INDEX.json", "assets\\POI_JSON_API\\zh_CN")
+
+def create_name():
+    s = []
+    for ii in range(1,14):
+        j = load_json(json_name=str(ii)+".json", default_path="assets\\POI_JSON_API\\zh_CN\\dataset")
+        for i in j:
+            s.append(i["markerTitle"])
+    s=list(set(s))
+    save_json(s, "ITEM_NAME.json", "assets\\POI_JSON_API\\zh_CN")
+
+reshape_json()
+create_indexes()
+create_name()
