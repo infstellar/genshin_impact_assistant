4e3fcce859e2ff7d26729d9e833caf65c816f104
LmeSzinc <37934724+LmeSzinc@users.noreply.github.com>
2023-02-11 22:33:06 +0800

    Opt: Change dir to GIA root
    Former-commit-id: da3ece8a37fe7dd1b988e1ece194120247bcc653

--- source/path_lib.py
+++ source/path_lib.py
@@ -1,11 +1,13 @@
-import os,sys
+import os, sys
+
 root_path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+os.chdir(root_path)
 source_path = root_path + '\\source'
 assets_path = root_path + '\\assets'
 if sys.path[0] != root_path:
     sys.path.insert(0, root_path)
 if sys.path[1] != source_path:
     sys.path.insert(1, source_path)
-    
-CONFIGPATH_SETTING = os.path.join(root_path,"config\\settings")
+
+CONFIGPATH_SETTING = os.path.join(root_path, "config\\settings")
 JSONNAME_CONFIG = "config.json"
