64076b75dd4b628a021b1d2d16beb500ad138a3b
infstellar <98541813+infstellar@users.noreply.github.com>
2023-02-15 22:28:03 +0800

    task init
    Former-commit-id: 60057437fe037b94eccd056a2714046a92b3edcb

---
+++ source/task/domain_task.py
@@ -0,0 +1,32 @@
+from source.util import *
+from source.flow.domain_flow_upgrad import DomainFlowController
+from source.task.task_template import TaskTemplate
+
+
+class DomainTask(TaskTemplate):
+    def __init__(self):
+        super().__init__()
+        self.dfc = DomainFlowController()
+        
+    def run(self) -> None:
+        '''if you're using this class, copy this'''
+        while 1:
+            time.sleep(self.while_sleep)
+            if self.stop_threading_flag:
+                return 0
+
+            if self.pause_threading_flag:
+                if self.working_flag:
+                    self.working_flag = False
+                time.sleep(1)
+                continue
+
+            if not self.working_flag:
+                self.working_flag = True
+                
+            if self.checkup_stop_func():
+                self.pause_threading_flag = True
+                continue
+            '''write your code below'''
+
+            
