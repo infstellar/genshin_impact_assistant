39e686e3b156cf18bce541d958cd13c60fe29fd6
infstellar <2644641825@qq.com>
2023-02-17 12:58:19 +0800

    add: tmfupd
    Former-commit-id: 6e4d9d328979ad1284acb0737c11be08b3325168

--- source/flow/teyvat_move_flow_upgrad.py
+++ source/flow/teyvat_move_flow_upgrad.py
@@ -1,6 +1,7 @@
 from source.util import *
 import math
 from source.constant import flow_state as ST
+import source.flow.flow_code as FC
 from source.base import timer_module
 from funclib import big_map, movement, static_lib, combat_lib
 from source.manager import scene_manager, posi_manager, asset
@@ -23,6 +24,20 @@ class TeyvatMoveFlowConnector(FlowConnector):
         self.tmc = teyvat_move_controller.TeyvatMoveController()
         self.target_posi = None
         self.checkup_stop_func = None
+        self.stop_rule = 0
+        self.tmc = teyvat_move_controller.TeyvatMoveController()
+        self.tmc.setDaemon(True)
+        self.tmc.add_stop_func(self.checkup_stop_func)
+        self.tmc.pause_threading()
+        self.tmc.start()
+        self.tmc.set_stop_rule(self.stop_rule)
+        self.jump_timer = timer_module.Timer()
+        self.current_state = ST.INIT_TEYVAT_TELEPORT
+        self.target_posi = [0, 0]
+        self.reaction_to_enemy = 'RUN'
+        self.motion_state = IN_MOVE
+    
+    
 
 
 
@@ -41,6 +56,9 @@ class TeyvatTeleport(FlowTemplate):
         self._next_rfc()
 
     def state_in(self):
+        """
+        这个代码是垃圾 之后大地图坐标识别模块接入之后要重写
+        """
         curr_posi = generic_event.cvAutoTrackerLoop.get_position()[1:]
         scene_lib.switch_to_page(scene_manager.page_bigmap, self.upper.checkup_stop_func)
         # Obtain the coordinates of the transmission anchor closest to the target coordinates
@@ -54,42 +72,137 @@ class TeyvatTeleport(FlowTemplate):
             logger.info(t2t("获取传送锚点失败，正在重试"))
             big_map.reset_map_size()
         global_itt.move_and_click([tw_posi[0], tw_posi[1]])
-        # self.itt.delay(0.2)
-        # self.itt.left_click()
-        # self.itt.delay(0.6)
+        # global_itt.delay(0.2)
+        # global_itt.left_click()
+        # global_itt.delay(0.6)
         temporary_timeout_1 = timer_module.TimeoutTimer(25)
         while 1:
-            if self.checkup_stop_func():
+            if self.upper.checkup_stop_func():
                 break
             
-            if self.itt.appear_then_click(asset.bigmap_tp) : break
+            if global_itt.appear_then_click(asset.bigmap_tp) : break
             if check_mode == 1:
                 logger.debug("tp to tw")
-                self.itt.appear_then_click(asset.CSMD)
+                global_itt.appear_then_click(asset.CSMD)
             else:
                 logger.debug("tp to ss")
-                self.itt.appear_then_click(asset.QTSX)
+                global_itt.appear_then_click(asset.QTSX)
             if temporary_timeout_1.istimeout():
-                scene_lib.switch_to_page(scene_manager.page_bigmap, self.checkup_stop_func)
-                self.itt.move_and_click([tw_posi[0], tw_posi[1]])
+                scene_lib.switch_to_page(scene_manager.page_bigmap, self.upper.checkup_stop_func)
+                global_itt.move_and_click([tw_posi[0], tw_posi[1]])
                 temporary_timeout_1.reset()
-            time.sleep(1)
-            # p1 = pdocr_api.ocr.get_text_position(self.itt.capture(jpgmode=0, posi=img_manager.bigmap_choose_area.cap_posi), "七天神像", cap_posi_leftup=img_manager.bigmap_choose_area.cap_posi[:2])
+            time.sleep(0.5)
+            # p1 = pdocr_api.ocr.get_text_position(global_itt.capture(jpgmode=0, posi=img_manager.bigmap_choose_area.cap_posi), "七天神像", cap_posi_leftup=img_manager.bigmap_choose_area.cap_posi[:2])
             # if p1 != -1:
-            #     self.itt.move_and_click([p1[0] + 30, p1[1] + 30], delay=1)
+            #     global_itt.move_and_click([p1[0] + 30, p1[1] + 30], delay=1)
             
-            # p1 = pdocr_api.ocr.get_text_position(self.itt.capture(jpgmode=0, posi=img_manager.bigmap_choose_area.cap_posi), "传送锚点", cap_posi_leftup=img_manager.bigmap_choose_area.cap_posi[:2])
+            # p1 = pdocr_api.ocr.get_text_position(global_itt.capture(jpgmode=0, posi=img_manager.bigmap_choose_area.cap_posi), "传送锚点", cap_posi_leftup=img_manager.bigmap_choose_area.cap_posi[:2])
             # if p1 != -1:
-            #     self.itt.move_and_click([p1[0] + 30, p1[1] + 30], delay=1)
+            #     global_itt.move_and_click([p1[0] + 30, p1[1] + 30], delay=1)
 
-        self.itt.move_and_click([posi_manager.tp_button[0], posi_manager.tp_button[1]], delay=1)
+        global_itt.move_and_click([posi_manager.tp_button[0], posi_manager.tp_button[1]], delay=1)
         
-        while not self.itt.get_img_existence(asset.ui_main_win):
-            if self.checkup_stop_func():
+        while not global_itt.get_img_existence(asset.ui_main_win):
+            if self.upper.checkup_stop_func():
                 break
             time.sleep(1)
         while generic_event.cvAutoTrackerLoop.in_excessive_error:
-            if self.checkup_stop_func():
+            if self.upper.checkup_stop_func():
                 break
             time.sleep(1)
-        self.current_state = ST.AFTER_TEYVAT_TELEPORT
+        self._next_rfc()
+
+    def state_after(self):
+        """也是垃圾"""
+        scene_lib.switch_to_page(scene_manager.page_main, self.upper.checkup_stop_func)
+        time.sleep(2)
+        curr_posi = generic_event.cvAutoTrackerLoop.get_position()[1:]
+        scene_lib.switch_to_page(scene_manager.page_bigmap, self.upper.checkup_stop_func)
+        tw_posi = big_map.nearest_teyvat_tw_posi(curr_posi, self.upper.target_posi, self.upper.checkup_stop_func)
+        p1 = euclidean_distance(self.upper.target_posi, tw_posi)
+        p2 = euclidean_distance(self.upper.target_posi, curr_posi)
+        if p1 < p2:
+            scene_lib.switch_to_page(scene_manager.page_main, self.upper.checkup_stop_func)
+            global_itt.delay(1)
+            self.rfc = FC.BEFORE
+        else:
+            self._next_rfc()
+
+    def state_end(self):
+        scene_lib.switch_to_page(scene_manager.page_main, self.upper.checkup_stop_func)
+        return super().state_end()
+class TeyvatMove(FlowTemplate):
+    def __init__(self, upper: TeyvatMoveFlowConnector):
+        super().__init__(upper)
+        self.upper = upper
+
+    def switch_motion_state(self):
+        if global_itt.get_img_existence(asset.motion_climbing):
+            self.motion_state = IN_CLIMB
+        elif global_itt.get_img_existence(asset.motion_flying):
+            self.motion_state = IN_FLY
+        elif global_itt.get_img_existence(asset.motion_swimming):
+            self.motion_state = IN_WATER
+        else:
+            self.motion_state = IN_MOVE
+
+
+
+    def state_init(self):
+        self.upper.tmc.continue_threading()
+        return super().state_init()
+
+    def state_in(self):
+        self.switch_motion_state()
+        
+        if self.motion_state == IN_MOVE:
+            if combat_lib.combat_statement_detection(global_itt):
+                '''进入战斗模式'''
+                if self.upper.reaction_to_enemy == 'RUN':
+                    '''越级执行护盾命令 还没想好怎么写'''
+                    pass
+                else:
+                    self.upper.tmc.pause_threading()
+            else:
+                self.upper.tmc.continue_threading()
+                if self.upper.jump_timer.get_diff_time()>=2:
+                    self.upper.jump_timer.reset()
+                    global_itt.key_press('spacebar')
+                    time.sleep(0.3)
+                    global_itt.key_press('spacebar') # fly
+            
+            
+                
+        if (self.motion_state == IN_FLY) or (self.motion_state == IN_CLIMB) or (self.motion_state == IN_WATER):
+            self.upper.tmc.continue_threading()
+            
+        if self.motion_state == IN_CLIMB:
+            if self.upper.jump_timer.get_diff_time()>=5:
+                self.upper.jump_timer.reset()
+                global_itt.key_press('spacebar')
+                time.sleep(0.3)
+                global_itt.key_press('spacebar') # fly    
+
+            '''可能会加体力条检测'''
+        # if self.stop_rule == 0:    
+        #     if euclidean_distance(generic_event.cvAutoTrackerLoop.get_position()[1:], self.target_posi)<=10:
+        #         self.current_state = ST.END_TEYVAT_MOVE
+        # elif self.stop_rule == 1:
+        #     if generic_lib.f_recognition():
+        #         self.current_state = ST.END_TEYVAT_MOVE
+        # elif self.
+
+        if self.upper.tmc.get_last_err_code() == ERR_PASS:
+            self.upper.tmc.reset_err_code()
+            self.last_err_code = ERR_PASS
+            self._next_rfc()
+        elif self.upper.tmc.get_last_err_code() == ERR_STUCK:
+            self.upper.tmc.reset_err_code()
+            self.last_err_code = ERR_STUCK
+            self._next_rfc()
+
+
+
+
+
+    
