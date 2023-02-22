d66580cc2ae7c1fbf7fd16e1c447e2c660186077
infstellar <2644641825@qq.com>
2023-02-22 18:27:18 +0800

    upd
    Former-commit-id: af5a1ee55c14df807ee83c80ee2dcf60a022f7b7

---
+++ source/operator/add_points_operator.py
@@ -0,0 +1,42 @@
+from source.util import *
+from source.common import generic_event
+from source.common.base_threading import BaseThreading
+
+
+class add_point(BaseThreading):
+
+    def __init__(self):
+        super().__init__()
+
+    def get_points(self):
+        generic_event.cvAutoTrackerLoop.get_position()[:2]
+        pass
+
+
+
+    def run(self):
+        '''if you're using this class, copy this'''
+        while 1:
+            time.sleep(self.while_sleep)
+            if self.stop_threading_flag:
+                return
+
+            if self.pause_threading_flag:
+                if self.working_flag:
+                    self.working_flag = False
+                time.sleep(1)
+                continue
+
+            if not self.working_flag:
+                self.working_flag = True
+                
+            if self.checkup_stop_func():
+                self.pause_threading_flag = True
+                continue
+        '''write your code below'''
+    
+
+
+
+if __name__ == '__main__':
+    pass
