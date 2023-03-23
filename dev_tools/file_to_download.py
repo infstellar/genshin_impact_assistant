84c9812e6598b80c153f3a90f8581f65bab381e8
infstellar <2644641825@qq.com>
2023-03-23 20:59:22 +0800

    feat: ftd

--- dev_tools/file_to_download.py
+++ dev_tools/file_to_download.py
@@ -1,6 +1,6 @@
 import shutil, os
-path1 = "D:\\Program Data\\vscode\\GIA\\forGIALDL\\genshin_impact_assistant_forLDL"
-path2 = "D:\\Program Data\\vscode\\GIA_Launcher_Download_Lib"
+path1 = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+path2 = os.path.abspath(r"../GIA_Launcher_Download_Lib")
 
 def verify_path(root):
     if not os.path.exists(root):
@@ -24,7 +24,20 @@ def copy_from_to(rootpath):
     #     if d not in [".git"]:
     #         copy_from_to(os.path.join(root, d))
 
+def del_files():
+    import os
+    import shutil
 
+    dirPath = os.path.abspath(r"../GIA_Launcher_Download_Lib") # 你要删除的文件夹路径
+    exceptFolder = ".git" # 你要保留的文件夹名称
 
+    for fileName in os.listdir(dirPath):
+        if fileName != exceptFolder:
+            filePath = os.path.join(dirPath, fileName)
+            if os.path.isfile(filePath):
+                os.remove(filePath) # 删除文件
+            elif os.path.isdir(filePath):
+                shutil.rmtree(filePath) # 删除文件夹
+
+del_files()
 copy_from_to(path1)
-                
