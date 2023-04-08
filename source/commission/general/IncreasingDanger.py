d7431bce58a29d04109fe36af825fa7c1609f56a
infstellar <98541813+infstellar@users.noreply.github.com>
2023-04-08 09:59:50 +0800

    feat: recognize distance to icon

--- source/commission/general/IncreasingDanger.py
+++ source/commission/general/IncreasingDanger.py
@@ -5,6 +5,7 @@ from source.util import *
 from source.interaction.interaction_core import itt
 from source.manager import asset
 from source.common.timer_module import AdvanceTimer
+from source.api.pdocr_light import ocr_light
 
 
 class IncreasingDangerGeneral(CommissionTemplate):
@@ -18,12 +19,25 @@ class IncreasingDangerGeneral(CommissionTemplate):
         r = movement.view_to_imgicon(cap, asset.CommissionIconInCommission)
         if not r:
             return False
-        else:
-            if r<=50:
-                itt.key_down('w')
-            else:
-                itt.key_up('w')
-            return r
+        if r<=15:
+            dist_cap = itt.capture([SCREEN_CENTER_X-80,SCREEN_CENTER_Y-100,SCREEN_CENTER_X+80,SCREEN_CENTER_Y+40],jpgmode=0)
+            dist_cap = extract_white_letters(dist_cap, threshold=90)
+            # is_num, dist = ocr.is_img_num_plus(dist_cap)
+            res = ocr_light.get_all_texts(dist_cap)
+            n=False
+            for i in res:
+                if 'm' in i:
+                    x=i.replace('m','')
+                    if is_int(x):
+                        n=int(i.replace('m',''))
+            if n:
+                print(n)
+                if n<=3: 
+                    itt.key_up('w')
+                else:
+                    itt.key_down('w')
+            return True
+        return False
     
     def exec_mission(self):
         attack_timer = AdvanceTimer(0.6)
@@ -34,7 +48,7 @@ class IncreasingDangerGeneral(CommissionTemplate):
             if self.checkup_stop_func():
                 self.stop_combat()
                 return
-            if self._aim_to_commission_icon()<=20:
+            if self._aim_to_commission_icon():
                 if attack_timer.reached_and_reset():itt.left_click()
             if self.is_commission_complete():
                 self.stop_combat()
@@ -45,6 +59,8 @@ class IncreasingDangerGeneral(CommissionTemplate):
 if __name__ == '__main__':
     idg = IncreasingDangerGeneral([3447.9764219999997,
       -4490.925582000001])
+    while 1:
+        idg._aim_to_commission_icon()
     idg.start()
     idg.continue_threading()
     while 1:
