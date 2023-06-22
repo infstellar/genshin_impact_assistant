48800dbbaa5cb6921680601368bf3621017eb7e4
infstellar <98541813+infstellar@users.noreply.github.com>
2023-06-22 16:45:02 +0800

    add: commission: LanguageExchange_P960N6796

--- source/commission/commission_index.py
+++ source/commission/commission_index.py
@@ -1,21 +1,30 @@
 """This file is generated automatically. Do not manually modify it."""
-COMMISSION_INDEX = {'ASmallStepForHilichurls_N1000P1000': {'type': 'ASmallStepForHilichurls', 'position': [-1000, 1000]}, 'BasicKnowledgeOfTheKnights_P2682N5673': {'type': 'BasicKnowledgeOfTheKnights', 'position': [2682, -5673]}, 'BigPudgyProblem_P2469N4886': {'type': 'BigPudgyProblem', 'position': [2469, -4886]}, 'BigPudgyProblem_P2526N5796': {'type': 'BigPudgyProblem', 'position': [2526, -5796]}, 'LanguageExchange_P3060N5078': {'type': 'LanguageExchange', 'position': [3060, -5078]}, 'PigeonsGoAWOL_P2671N5095': {'type': 'PigeonsGoAWOL', 'position': [2671, -5095]}, 'TheUnnoticedGuy_P2568N5889': {'type': 'TheUnnoticedGuy', 'position': [2568, -5889]}}
+COMMISSION_INDEX = {'BasicKnowledgeOfTheKnights_P2682N5673': {'type': 'BasicKnowledgeOfTheKnights', 'position': [2682, -5673]}, 'FloralFresh_P2556N5813': {'type': 'FloralFresh', 'position': [2556, -5813]}, 'FreshFlora_P2648N5726': {'type': 'FreshFlora', 'position': [2648, -5726]}, 'LanguageExchange_P3060N5078': {'type': 'LanguageExchange', 'position': [3060, -5078]}, 'LanguageExchange_P960N6796': {'type': 'LanguageExchange', 'position': [960, -6796]}, 'PigeonsGoAWOL_P2671N5095': {'type': 'PigeonsGoAWOL', 'position': [2671, -5095]}, 'ReliableHelper_P2966N4774': {'type': 'ReliableHelper', 'position': [2966, -4774]}, 'TheUnnoticedGuy_P2568N5889': {'type': 'TheUnnoticedGuy', 'position': [2568, -5889]}, 'TheWannabeKnight_P2251N5771': {'type': 'TheWannabeKnight', 'position': [2251, -5771]}}
 def get_commission_object(commission_name:str):
     if commission_name == 'BasicKnowledgeOfTheKnights_P2682N5673':
         import source.commission.commissions.BasicKnowledgeOfTheKnights_P2682N5673
         return source.commission.commissions.BasicKnowledgeOfTheKnights_P2682N5673.BasicKnowledgeOfTheKnights_P2682N5673()
-    if commission_name == 'BigPudgyProblem_P2469N4886':
-        import source.commission.commissions.BigPudgyProblem_P2469N4886
-        return source.commission.commissions.BigPudgyProblem_P2469N4886.BigPudgyProblem_P2469N4886()
-    if commission_name == 'BigPudgyProblem_P2526N5796':
-        import source.commission.commissions.BigPudgyProblem_P2526N5796
-        return source.commission.commissions.BigPudgyProblem_P2526N5796.BigPudgyProblem_P2526N5796()
+    if commission_name == 'FloralFresh_P2556N5813':
+        import source.commission.commissions.FloralFresh_P2556N5813
+        return source.commission.commissions.FloralFresh_P2556N5813.FloralFresh_P2556N5813()
+    if commission_name == 'FreshFlora_P2648N5726':
+        import source.commission.commissions.FreshFlora_P2648N5726
+        return source.commission.commissions.FreshFlora_P2648N5726.FreshFlora_P2648N5726()
     if commission_name == 'LanguageExchange_P3060N5078':
         import source.commission.commissions.LanguageExchange_P3060N5078
         return source.commission.commissions.LanguageExchange_P3060N5078.LanguageExchange_P3060N5078()
+    if commission_name == 'LanguageExchange_P960N6796':
+        import source.commission.commissions.LanguageExchange_P960N6796
+        return source.commission.commissions.LanguageExchange_P960N6796.LanguageExchange_P960N6796()
     if commission_name == 'PigeonsGoAWOL_P2671N5095':
         import source.commission.commissions.PigeonsGoAWOL_P2671N5095
         return source.commission.commissions.PigeonsGoAWOL_P2671N5095.PigeonsGoAWOL_P2671N5095()
+    if commission_name == 'ReliableHelper_P2966N4774':
+        import source.commission.commissions.ReliableHelper_P2966N4774
+        return source.commission.commissions.ReliableHelper_P2966N4774.ReliableHelper_P2966N4774()
     if commission_name == 'TheUnnoticedGuy_P2568N5889':
         import source.commission.commissions.TheUnnoticedGuy_P2568N5889
         return source.commission.commissions.TheUnnoticedGuy_P2568N5889.TheUnnoticedGuy_P2568N5889()
+    if commission_name == 'TheWannabeKnight_P2251N5771':
+        import source.commission.commissions.TheWannabeKnight_P2251N5771
+        return source.commission.commissions.TheWannabeKnight_P2251N5771.TheWannabeKnight_P2251N5771()
