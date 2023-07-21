d7c6f18d4b8bd01220e0bbfac42b1d373e3cb12a
infstellar <98541813+infstellar@users.noreply.github.com>
2023-07-21 10:32:50 +0800

    fix: req

--- source/ui/page.py
+++ source/ui/page.py
@@ -1,8 +1,8 @@
 import traceback
 from source.manager import asset, img_manager
 
-class UIPage():
 
+class UIPage():
     parent = None
 
     def __init__(self, check_icon: img_manager.ImgIcon):
@@ -27,23 +27,23 @@ class UIPage():
         button:Button/Str
         """
         self.links[destination] = button
-        
+
     def is_current_page(self, itt, print_log=False):
         for imgicon in self.check_icon_list:
             ret = itt.get_img_existence(imgicon, is_log=print_log)
             if ret:
                 return True
         return False
-    
+
     def add_check_icon(self, check_icon: img_manager.ImgIcon):
         self.check_icon_list.append(check_icon)
 
 
-page_main = UIPage(check_icon = asset.IconUIEmergencyFood)
-page_esc = UIPage(check_icon = asset.IconUIEscMenu)
-page_domain = UIPage(check_icon = asset.IconUIInDomain)
-page_bigmap = UIPage(check_icon = asset.IconUIBigmap)
-page_time = UIPage(check_icon = asset.IconUITimeMenuCore)
+page_main = UIPage(check_icon=asset.IconUIEmergencyFood)
+page_esc = UIPage(check_icon=asset.IconUIEscMenu)
+page_domain = UIPage(check_icon=asset.IconUIInDomain)
+page_bigmap = UIPage(check_icon=asset.IconUIBigmap)
+page_time = UIPage(check_icon=asset.IconUITimeMenuCore)
 page_configure_team = UIPage(check_icon=asset.IconUIPartySetup)
 
 page_main.link('m', page_bigmap)
@@ -54,4 +54,3 @@ page_esc.link(asset.ButtonUIEnterPartySetup, page_configure_team)
 page_time.link(asset.ButtonGeneralExit, page_esc)
 page_bigmap.link('m', page_main)
 page_configure_team.link('esc', page_esc)
-
