5da8faf1e0644e4623ff7e102b6eb807b57757a5
infstellar <98541813+infstellar@users.noreply.github.com>
2023-12-13 23:32:51 +0800

    feat: i18n & note in combat setting page

--- source/webio/webpages/combat_settings.py
+++ source/webio/webpages/combat_settings.py
@@ -5,25 +5,23 @@ from source.webio.webpages.config import ConfigPage
 from source.config.cvars import *
 
 
-
-
 class CombatSettingPage(ConfigPage):
     def __init__(self):
-        super().__init__(config_file_name = CONFIGNAME_COMBAT)
+        super().__init__(config_file_name=CONFIGNAME_COMBAT)
         from source.common.lang_data import get_all_characters_name
         self.character_names = get_all_characters_name()
-        self.input_verify={
-            "character_name":self.character_names
+        self.input_verify = {
+            "character_name": self.character_names
         }
-        
+
     def _autofill(self):
         j = self.get_json(json.load(open(self.file_name, 'r', encoding='utf8')))
         for i in j:
             if 'long_attack_time' not in i:
-                j[i]["long_attack_time"]=2.5
+                j[i]["long_attack_time"] = 2.5
         autofill_j = load_json("characters_parameters.json", f"{ASSETS_PATH}\\characters_data")
         not_found = []
-            
+
         from source.common.lang_data import translate_character_auto
         for i in j:
             cname = translate_character_auto(j[i]["name"])
@@ -31,18 +29,19 @@ class CombatSettingPage(ConfigPage):
                 not_found.append(j[i]["name"])
                 continue
             if cname in autofill_j:
-                for k in ["position", "E_short_cd_time", "E_long_cd_time", "Elast_time", "Epress_time", "tactic_group", "trigger", "Qlast_time", "Qcd_time", "vision", "long_attack_time"]:
+                for k in ["position", "E_short_cd_time", "E_long_cd_time", "Elast_time", "Epress_time", "tactic_group",
+                          "trigger", "Qlast_time", "Qcd_time", "vision", "long_attack_time"]:
                     if j[i][k] == "" or j[i][k] == -1:
                         j[i][k] = autofill_j[cname][k]
             else:
                 not_found.append(cname)
         output.clear_scope('now')
-        if len(not_found)==0:
+        if len(not_found) == 0:
             output.popup("自动填充", "自动填充成功\n以下选项不会自动填充:\n优先级,角色在队伍中的位置。\n记得保存(￣▽￣)~*")
         else:
-            output.popup("自动填充", 
-                         "自动填充部分失败\n"+
-                         str(not_found)+"\n"+
+            output.popup("自动填充",
+                         "自动填充部分失败\n" +
+                         str(not_found) + "\n" +
                          "以下选项不会自动填充:\n优先级,角色在队伍中的位置。\n记得保存(￣▽￣)~*")
         self.put_setting(name=self.file_name, j=j)
 
@@ -52,7 +51,8 @@ class CombatSettingPage(ConfigPage):
             for f in files:
                 if f[f.index('.') + 1:] == "json":
                     self.config_files.append({"label": f, "value": os.path.join(root, f)})
-        self.config_files.append({"label": f"{CONFIGNAME_COMBAT}.json", "value": fr"config/settings/{CONFIGNAME_COMBAT}.json"})
+        self.config_files.append(
+            {"label": f"{CONFIGNAME_COMBAT}.json", "value": fr"config/settings/{CONFIGNAME_COMBAT}.json"})
 
     def _load(self):
         self.last_file = None
@@ -70,7 +70,8 @@ class CombatSettingPage(ConfigPage):
         # 配置页
         output.put_markdown(t2t('## config:'), scope=self.main_scope)
         output.put_scope("select_scope", scope=self.main_scope)
-        pin.put_select('file', self._config_file2lableAfile(self.config_files), scope="select_scope", value="config/settings/Combat.json")
+        pin.put_select('file', self._config_file2lableAfile(self.config_files), scope="select_scope",
+                       value="config/settings/Combat.json")
 
     def onclick_add_teamjson(self):
         n = input.input('team name')
@@ -83,4 +84,6 @@ class CombatSettingPage(ConfigPage):
         shutil.copy(os.path.join(ROOT_PATH, "config\\tactic\\team_with_characters.uijsontemplate"),
                     os.path.join(ROOT_PATH, "config\\tactic", n + '.json'))
         self._reload_select()
+        output.toast(t2t('Note: After creating a new tactic file, you must fill in all blank input boxes or `-1` input boxes'))
         pass
+
