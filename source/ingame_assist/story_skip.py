9f3c29346ecbc880af03677d01f3284dbae79a4c
infstellar <98541813+infstellar@users.noreply.github.com>
2024-07-24 21:02:29 +0800

    opt: story_skip stop when not in the talk #145 #103

--- source/ingame_assist/story_skip.py
+++ source/ingame_assist/story_skip.py
@@ -5,7 +5,10 @@ class StorySkip(AdvanceThreading, Talk):
     def __init__(self, thread_name=None):
         super().__init__(thread_name)
         self.while_sleep = 0.5
-    
+
+    def stop_rule(self):
+        return self.checkup_stop_func() or (not self._is_in_talking())
+
     def loop(self):
         if self._is_in_talking():
-            self.talk_skip(self.checkup_stop_func)
+            self.talk_skip(self.stop_rule)
