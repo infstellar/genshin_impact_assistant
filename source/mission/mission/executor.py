293fae6fab5b9c9b1223d7ab778f878df8e4a487
infstellar <98541813+infstellar@users.noreply.github.com>
2024-07-24 21:04:36 +0800

    refactor: remove mission.mission package

--- source/mission/mission/executor.py
+++
@@ -1,5 +0,0 @@
-from source.mission.mission.util import *
-from source.mission.mission.base import MissionBase
-
-class MissionExecutor(MissionBase):
-    pass
--- source/mission/mission/util.py
+++
@@ -1,1 +0,0 @@
-from source.util import *
